eea483f149d23c034509bef17b0caaeeb23d99b3
purplesunset871019 <hsb41202@gmail.com>
2019-10-14 23:19:35 +0800

    gridview嘗試

--- Rehabilitation/Rehabilitation/Doctor.aspx.cs
+++ Rehabilitation/Rehabilitation/Doctor.aspx.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Rehabilitation
 {
     public partial class Doctor : System.Web.UI.Page
     {
+        String connectionString = @"Data Sourse = (local)\aqle2012; Initial Catelog = PhoneBookDB; Integrated Securlty=True;";
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM PhoneBook", sqlCon);
+                DataTable dtb1 = new DataTable();
+                sqlDa.Fill(dtb1);
+                gvPhoneBook.DataSource = dtb1;
+                gvPhoneBook.DataBind();
+            }
         }
 
         protected void Doctor_sign_out_Click(object sender, EventArgs e)
@@ -19,9 +30,5 @@ namespace Rehabilitation
 
         }
 
-        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
     }
 }
--- Rehabilitation/Rehabilitation/Doctor.aspx.designer.cs
+++ Rehabilitation/Rehabilitation/Doctor.aspx.designer.cs
@@ -22,13 +22,13 @@ namespace Rehabilitation {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// GridView1 控制項。
+        /// gvPhoneBook 控制項。
         /// </summary>
         /// <remarks>
         /// 自動產生的欄位。
         /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
+        protected global::System.Web.UI.WebControls.GridView gvPhoneBook;
         
         /// <summary>
         /// Button1 控制項。
